47e3399bf2bb8ab848bbdd5cf6c3d4f383a318d5
taweesak <taweesak9359@gmail.com>
2020-02-06 20:07:22 +0700

    update1

---
+++ app/src/main/java/com/example/myappsspgdmanagev1/ShowItemActivity.java
@@ -0,0 +1,14 @@
+package com.example.myappsspgdmanagev1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_item);
+    }
+}
